c611dc1c17c503e84f8d95900bc8c145a88a2b24
Calle <derpninjas88rgmail.com>
2022-11-25 10:32:58 +0100

    Int commit

---
+++ Uppgift6/skript.php
@@ -0,0 +1,38 @@
+<?php
+ 
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "webbserverprogrammering";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+$login_success = false;
+$full_name = "";
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+		if($row["username"] == $_POST["username"] &&
+					$row["password"] == $_POST["password"]) {
+			$login_success = true;
+            echo "Välkommen ", $_POST["username"];
+                    }
+        else{
+            echo "login failed";
+        }
+	}
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+if($login_success) {
+	session_start();
+	$_SESSION["username"] = $_POST["username"];
+}
+ echo "<a href='upload.html'>Ladda upp fil</a>";
+
+
